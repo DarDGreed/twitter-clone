e9b35d0acff343f0b3ec1c34d4437de08db7c3d0
ShadyDar <141896824+DarDGreed@users.noreply.github.com>
2025-04-14 08:09:11 +0800

    like post mutation added

--- server/controller/post.controller.js
+++ server/controller/post.controller.js
@@ -140,7 +140,9 @@ export const likeUnlikePost = async (req, res) => {
     if(userLikedPost){
       await Post.updateOne({_id:postId},{$pull:{likes: userId}})
       await User.updateOne({_id:userId},{$pull: {likedPosts: postId}})
-      res.status(200).json({message: "Post unliked successfully"})
+
+      const updatedLikes = post.likes.filter((id) => id.toString !== userId.toString())
+      res.status(200).json(updatedLikes)
     }else{
       post.likes.push(userId)
       await User.updateOne({_id:userId},{$push: {likedPosts: postId}})
@@ -154,7 +156,8 @@ export const likeUnlikePost = async (req, res) => {
 
       await notification.save()
 
-      res.status(200).json({ message: " Post Liked successfully" })
+      const updatedLikes = post.likes
+       res.status(200).json(updatedLikes)
     }
 
   } catch (error) {
